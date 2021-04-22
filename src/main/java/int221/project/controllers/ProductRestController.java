3d3a62514f18a1d6e6b087ede155cdad93583da2
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-04-22 16:35:56 +0700

    [Update] ProductRestController

--- src/main/java/int221/project/controllers/ProductRestController.java
+++ src/main/java/int221/project/controllers/ProductRestController.java
@@ -1,5 +1,43 @@
 package int221.project.controllers;
 
-public class ProductRestController {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import int221.project.models.Product;
+import int221.project.repositories.ProductRepository;
+
+@RestController
+public class ProductRestController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping("/{id}")
+	public Product product(@PathVariable String id) {
+		return productRepository.findById(id).orElse(null);
+	}
+
+	@PostMapping("")
+	public Product create(@RequestBody Product newProduct) {
+		productRepository.save(newProduct);
+		return newProduct;
+	}
+	
+	@PutMapping("")
+	public Product update(@RequestBody Product newProduct) {
+		productRepository.save(newProduct);
+		return newProduct;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable String id) {
+		productRepository.deleteById(id);
+	}
 }
